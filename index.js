d5659a65ad230370db255ce53b75e9071b2780ce
RyanFi <ryanmcmahon94@gmail.com>
2021-03-26 10:21:03 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+//declare variable customerName with var, set equal to bob
+var customerName = 'bob';
+
+//declare variable leastFavoriteCustomer with const, equal whatever
+const leastFavoriteCustomer = 'ryan';
+
+//create function that takes the name and uppercases
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+//create function that declares bestCustomer variable in global scope 
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+//write new function to overwrite best customer
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+//write function that tries to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'paul';
+}
